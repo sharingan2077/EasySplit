6f5b472bcaf24b2d704cc613c7cde3349c8cef58
Maksim <114164405+sharingan2077@users.noreply.github.com>
2023-05-27 10:29:21 +0300

    final

--- app/src/main/java/com/example/easysplit/view/activities/MainActivity.java
+++ app/src/main/java/com/example/easysplit/view/activities/MainActivity.java
@@ -108,6 +108,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void setTransparentStatusBar()
     {
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
         setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false);
         getWindow().setStatusBarColor(Color.TRANSPARENT);
     }
